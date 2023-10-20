bc2008d52ef6f9c9219533d27ee064c3115aaaa9
matheus899 <105080795+matheus899@users.noreply.github.com>
2023-10-20 11:20:50 -0300

    cd

--- main.js
+++ main.js
@@ -14,3 +14,8 @@ function tocaSomuepa(){
 }
 //chamda ou invocação da função tocaSomkiko ao clicar no botão
 document.querySelector('.tecla_uepa').onlick = tocapom;
+
+//criando a referências constante listaDeTeclas e busando a classe coletiva .tecla
+const listaDeTeclas = document.querySelectorAll('.tecla');
+//chamando a constante listaDeTeclas e o índice "0" do somPom
+listaDeTecals [0].onlick = tocaSomPom;
