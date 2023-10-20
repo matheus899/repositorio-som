20aaf64835f039cac27ec9ffadc28140eac90d17
matheus899 <105080795+matheus899@users.noreply.github.com>
2023-10-20 10:55:11 -0300

    vv

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ function tocaSomkiko(){
 }
  //chamda ou invocação da função tocaSomkiko ao clicar no botão
 document.querySelector('.tecla_pom').onlick = tocapom;
+
+//Declaração/criação da função tocaSomuepa
+function tocaSomuepa(){
+    document.queryselector ('#som_tecla_uepa').play
+
+}
+//chamda ou invocação da função tocaSomkiko ao clicar no botão
+document.querySelector('.tecla_uepa').onlick = tocapom;
