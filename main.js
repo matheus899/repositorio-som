19ce950fc9af741c52129852be7994208519c8c9
matheus899 <105080795+matheus899@users.noreply.github.com>
2023-10-20 10:45:19 -0300

    fdfdf

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert(‘Olá, mundo’);
+
+//Declaração/criação da função tocaSomkiko
+function tocaSomkiko(){
+     document.queryselector ('#som_tecla_pom').play
+
+}
+ //chamda ou invocação da função tocaSomkiko ao clicar no botão
+document.querySelector('.tecla_pom').onlick = tocapom;
