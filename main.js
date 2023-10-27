5011ab369754fe895d65143606142e30f8d1ac31
matheus899 <105080795+matheus899@users.noreply.github.com>
2023-10-27 10:22:00 -0300

    f

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 
-//Declaração/criação da função tocaSomkiko
-function tocaSomkiko(){
-     document.queryselector ('#som_tecla_pom').play
-
-}
- //chamda ou invocação da função tocaSomkiko ao clicar no botão
-document.querySelector('.tecla_pom').onlick = tocapom;
 
 //Declaração/criação da função tocaSomuepa
 function tocaSomuepa(){
     document.queryselector ('#som_tecla_uepa').play
 
 }
-//chamda ou invocação da função tocaSomkiko ao clicar no botão
-document.querySelector('.tecla_uepa').onlick = tocapom;
 
 //criando a referências constante listaDeTeclas e busando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 //chamando a constante listaDeTeclas e o índice "0" do somPom
 listaDeTecals [0].onlick = tocaSomPom;
+
+let contador = 0;
+while (contador <9){
+      listadeteclados[0]. olick = tocasomkiko
+
+}
