fac6b1f1899ece318d4b18040683acd3d1f31f0d
matheus899 <105080795+matheus899@users.noreply.github.com>
2023-10-31 14:23:04 -0300

    F

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ let contador = 0;
 while(contador < listaDeTeclas.length){
    const efeito= listaDeTeclas[contador] . classList[1];
    const idAudio= ("#som_") + efeito;
-      listadeTeclas[contador]. onclick = function() {
+      listaDeTeclas[contador]. onclick = function() {
         tocasom(idAudio);
       }
       contador = contador + 1;
