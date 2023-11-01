e33a97af64b7f58f70b9c01d0f87133971e0e1f8
matheus899 <105080795+matheus899@users.noreply.github.com>
2023-10-31 21:59:00 -0300

    D

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
-
-
-//Declaração/criação da função tocaSom
-function tocaSom (idElementoAudio){
-    document.queryselector (idElementoAudio).play();
-
+//Declaração/criação da função
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
-//criando a referências constante listaDeTeclas e busando a classe coletiva .tecla
+//criando a referência constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
 let contador = 0;
 while(contador < listaDeTeclas.length){
-   const efeito= listaDeTeclas[contador] . classList[1];
-   const idAudio= ("#som_") + efeito;
-      listaDeTeclas[contador]. onclick = function() {
-        tocaSom (idAudio);
-      }
-      contador = contador + 1;
-      console.log(contador);
-
-}
+  const efeito = listaDeTeclas[contador].classList[1];
+  const idAudio = "#som_" + efeito;
+  listaDeTeclas[contador].onclick = function(){
+      tocaSom(idAudio);
+  }
+  contador = contador + 1;
+  console.log(contador);
