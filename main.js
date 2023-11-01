28ec7b4f5c8e105ad28dcb376d0129ba3ddbcda7
matheus899 <105080795+matheus899@users.noreply.github.com>
2023-10-31 21:56:41 -0300

    D

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ while(contador < listaDeTeclas.length){
    const efeito= listaDeTeclas[contador] . classList[1];
    const idAudio= ("#som_") + efeito;
       listaDeTeclas[contador]. onclick = function() {
-        tocasom(idAudio);
+        tocaSom(idAudio);
       }
       contador = contador + 1;
       console.log(contador);
