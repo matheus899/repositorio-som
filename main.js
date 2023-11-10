49e4143990f8b8cc133e3ecd3f7e43a67cb07651
matheus899 <105080795+matheus899@users.noreply.github.com>
2023-11-10 11:08:35 -0300

    F

--- main.js
+++ main.js
@@ -6,14 +6,12 @@ function tocaSom (idElementoAudio){
 //criando a referência constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
+//criação da repetição "while" e trocado pelo "for"com variavel,condição e incremento
 for(let contador = 0;contador < listaDeTeclas.length; contador = contador ++){
-  const teclas = listaDeTeclas[contador]
-  const efeito = teclas.classList[1];
-  const idAudio = `#som_${efeito}`;
-  teclas.onlick = function(){
-    tocasom(idAudio);
+  const teclas = listaDeTeclas[contador]; //guarda o contador da lista 
+  const efeito = teclas.classList[1]; //chama 0 2º item classe individual do som 
+  const idAudio = `#som_${efeito}`; //utilizaa junção teplate string ${}
+  teclas.onlick = function(){      //função anônima auxiliar para chamar tocaSom
+    tocasom(idAudio);            //chamada da função tocaSom com parâmetro idAUDIO
   }
-
-  console.log(contador);
 }
