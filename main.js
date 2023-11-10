f2242a25e0f474cdf88c04d27b330082edc02e6f
matheus899 <105080795+matheus899@users.noreply.github.com>
2023-11-10 10:53:05 -0300

    ddd

--- main.js
+++ main.js
@@ -8,9 +8,10 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 while(contador < listaDeTeclas.length){
-  const efeito = listaDeTeclas[contador].classList[1];
-  const idAudio = "#som_" + efeito;
-  listaDeTeclas[contador].onclick = function(){
+  const teclas = listaDeTeclas[contador]
+  const efeito = teclas.classList[1];
+  const idAudio = "#som_" ${efeito};
+  teclas.onlick = function(){
     tocasom(idAudio);
   };
   contador = contador + 1;
